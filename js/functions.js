30330d4c90abb397e60a0964f87acbe8deae5cbc
Francesco Maestroni <francescomaestroni1@gmail.com>
2020-12-29 11:53:59 +0100

    Refactoring

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,25 @@
+function handleCellClick(e){
+
+    if (e.target.innerHTML !== ""){
+      return
+    }
+
+    if (gameIsLive === false){
+      return
+    }
+
+    if (xIsNext === true && e.target.innerHTML === ""){
+      e.target.innerHTML = X_SYMBOL;
+      e.target.classList.remove("overlay");
+      checkGameStatus();
+    } else if (xIsNext === false && e.target.innerHTML === "") {
+      e.target.innerHTML = O_SYMBOL;
+      e.target.classList.remove("overlay");
+      checkGameStatus();
+    }
+
+}
+
 function checkGameStatus(){
   const topLeft = cellsEl[0].innerHTML;
   const topCenter = cellsEl[1].innerHTML;
@@ -9,70 +31,81 @@ function checkGameStatus(){
   const bottomCenter = cellsEl[7].innerHTML;
   const bottomRight = cellsEl[8].innerHTML;
 
-  // console.log(topLeft, topCenter, topRight, middleLeft, middleCenter, middleRight, bottomLeft, bottomCenter, bottomRight);
-
   // Winning Combinations
 
   if (topLeft !== "" && topLeft === topCenter && topLeft === topRight) {
-    gameIsLive = false;
-    winner = topLeft;
-    console.log("vincitore: ", winner);
-    playerStatus[0].textContent = "Giocatore " +  winner + " ha vinto!";
+    handleWin(topLeft);
+    cellsEl[0].classList.add("red-text");
+    cellsEl[1].classList.add("red-text");
+    cellsEl[2].classList.add("red-text");
   } else if (middleLeft !== "" && middleLeft === middleCenter && middleLeft === middleRight) {
-    gameIsLive = false;
-    winner = middleLeft;
-    console.log("vincitore: ", winner);
-    playerStatus[0].textContent = "Giocatore " +  winner + " ha vinto!";
+    handleWin(middleLeft);
+    cellsEl[3].classList.add("red-text");
+    cellsEl[4].classList.add("red-text");
+    cellsEl[5].classList.add("red-text");
   } else if (bottomLeft !== "" && bottomLeft === bottomCenter && bottomLeft === bottomRight) {
-    gameIsLive = false;
-    winner = bottomLeft;
-    console.log("vincitore: ", winner);
-    playerStatus[0].textContent = "Giocatore " +  winner + " ha vinto!";
+    handleWin(bottomLeft);
+    cellsEl[6].classList.add("red-text");
+    cellsEl[7].classList.add("red-text");
+    cellsEl[8].classList.add("red-text");
   } else if (topLeft !== "" && topLeft === middleLeft && topLeft === bottomLeft) {
-    gameIsLive = false;
-    winner = topLeft;
-    console.log("vincitore: ", winner);
-    playerStatus[0].textContent = "Giocatore " +  winner + " ha vinto!";
+    handleWin(topLeft);
+    cellsEl[0].classList.add("red-text");
+    cellsEl[3].classList.add("red-text");
+    cellsEl[6].classList.add("red-text");
   } else if (topCenter !== "" && topCenter === middleCenter && topCenter === bottomCenter) {
-    gameIsLive = false;
-    winner = topCenter;
-    console.log("vincitore: ", winner);
-    playerStatus[0].textContent = "Giocatore " +  winner + " ha vinto!";
+    handleWin(topCenter);
+    cellsEl[1].classList.add("red-text");
+    cellsEl[4].classList.add("red-text");
+    cellsEl[7].classList.add("red-text");
   } else if (topRight !== "" && topRight === middleRight && topRight === bottomRight) {
-    gameIsLive = false;
-    winner = topRight;
-    console.log("vincitore: ", winner);
-    playerStatus[0].textContent = "Giocatore " +  winner + " ha vinto!";
+    handleWin(topRight);
+    cellsEl[2].classList.add("red-text");
+    cellsEl[5].classList.add("red-text");
+    cellsEl[8].classList.add("red-text");
   } else if (topLeft !== "" && topLeft === middleCenter && topLeft === bottomRight) {
-    gameIsLive = false;
-    winner = topLeft;
-    console.log("vincitore: ", winner);
-    playerStatus[0].textContent = "Giocatore " +  winner + " ha vinto!";
+    handleWin(topLeft);
+    cellsEl[0].classList.add("red-text");
+    cellsEl[4].classList.add("red-text");
+    cellsEl[8].classList.add("red-text");
   } else if (topRight !== "" && topRight === middleCenter && topRight === bottomLeft) {
-    gameIsLive = false;
-    winner = topRight;
-    console.log("vincitore: ", winner);
-    playerStatus[0].textContent = "Giocatore " +  winner + " ha vinto!";
+    handleWin(topRight);
+    cellsEl[2].classList.add("red-text");
+    cellsEl[4].classList.add("red-text");
+    cellsEl[6].classList.add("red-text");
   } else if (topLeft && topCenter && topRight && middleLeft && middleCenter && middleRight && bottomLeft && bottomCenter && bottomRight) {
     gameIsLive = false;
     playerStatus[0].textContent = "Pareggio!";
   } else {
-    xIsNext = !xIsNext;
-    if (xIsNext) {
-      playerStatus[0].textContent = "Tocca al Giocatore " + X_SYMBOL;
-    } else {
-      playerStatus[0].textContent = "Tocca al Giocatore " + O_SYMBOL;
-    }
+    switchTurn();
   }
+}
+
+function switchTurn(){
+  xIsNext = !xIsNext;
+  if (xIsNext) {
+    playerStatus[0].textContent = "Tocca al Giocatore " + X_SYMBOL;
+  } else {
+    playerStatus[0].textContent = "Tocca al Giocatore " + O_SYMBOL;
+  }
+}
+
+function handleWin(winningPlayer){
+  gameIsLive = false;
+  winner = winningPlayer;
+  console.log("vincitore: ", winner);
+  playerStatus[0].textContent = "Giocatore " +  winner + " ha vinto!";
 }
 
 function handleReset(){
   xIsNext = true;
+  gameIsLive = true;
   playerStatus[0].textContent = "Tocca al Giocatore " + X_SYMBOL;
 
   cellsEl.forEach(function(cell, index) {
     cell.innerHTML = "";
     cell.classList.add("overlay");
+    cell.classList.remove("red-text");
     winner = null;
   })
 }
--- js/main.js
+++ js/main.js
@@ -7,8 +7,6 @@ const resetBtn = document.getElementById("reset-btn");
 const playerStatus = document.getElementsByClassName("player-status");
 playerStatus[0].textContent = "Giocatore ✕";
 
-
-
 // Game Variables
 let xIsNext = true;
 let gameIsLive = true;
@@ -16,26 +14,7 @@ let winner = null;
 
 // Event Handlers
 cellsEl.forEach(function(cell, index){
-  cell.addEventListener("click", function (e){
-    // console.log("cell", e.target);
-
-    if (e.target.innerHTML !== ""){
-      return
-    }
-
-    if (xIsNext === true && e.target.innerHTML === ""){
-      e.target.innerHTML = X_SYMBOL;
-      e.target.classList.remove("overlay");
-      checkGameStatus();
-    } else if (xIsNext === false && e.target.innerHTML === "") {
-      e.target.innerHTML = O_SYMBOL;
-      e.target.classList.remove("overlay");
-      checkGameStatus();
-    }
-
-  // console.log(xIsNext);
-
-  })
+  cell.addEventListener("click", handleCellClick);
 })
 
 resetBtn.addEventListener("click", handleReset);
